2abc51104d462cd6f7801709537a4f776f94f3bd
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-06-17 17:30:58 +0200

    Update App.jsx

--- tiping-game/src/App.jsx
+++ tiping-game/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
+
+const [text, setText] = useState('')
+
+function handleChange(e){
+  setText(e.target.value)
+}
+
+function countWords(text){
+  const wordsArray = text.split(' ').filter(word => word !== '')
+  console.log(wordsArray)
+}
+
   return (
     <main>
-      <h1>Hello World</h1>
-      <textarea></textarea>
+      <h1>How fast can you type?</h1>
+      <textarea
+        onChange={handleChange}
+        value={text}
+      ></textarea>
       <h2>Time remaining: ???</h2>
-      <button>Start</button>
+      <button onClick={() => countWords(text)}>Start</button>
       <h2>Words counted: ???</h2>
     </main>
   )
