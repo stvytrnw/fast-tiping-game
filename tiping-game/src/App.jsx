717e7dabf29a2fe3b0c26a76a8aad6e19912d52f
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-06-17 17:05:56 +0200

    .

---
+++ tiping-game/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
+
+export default App
