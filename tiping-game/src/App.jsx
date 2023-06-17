07b0f3099f7aed4c7a14b21b72e56ffcd76efa52
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-06-17 17:08:22 +0200

    Update App.jsx

--- tiping-game/src/App.jsx
+++ tiping-game/src/App.jsx
@@ -4,6 +4,10 @@ function App() {
   return (
     <main>
       <h1>Hello World</h1>
+      <textarea></textarea>
+      <h2>Time remaining: ???</h2>
+      <button>Start</button>
+      <h2>Words counted: ???</h2>
     </main>
   )
 }
